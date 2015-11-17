eba23cf7ec4a9a597510c1ade54413ad8d40669a
Elizeu Castro <elizeuvoraf@gmail.com>
2015-11-17 01:15:50 -0400

    varregando página html e pegando informações por tag html

---
+++ poc/POC-GetDataFromHtml/src/LeiEstadual.java
@@ -0,0 +1,58 @@
+public class LeiEstadual {
+
+	String ano;
+	String numero;
+	String data;
+	String ementa;
+	String linkLei;
+	String linkLeiDetalhe;
+
+	public String getAno() {
+		return ano;
+	}
+
+	public void setAno(String ano) {
+		this.ano = ano;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getEmenta() {
+		return ementa;
+	}
+
+	public void setEmenta(String ementa) {
+		this.ementa = ementa;
+	}
+
+	public String getLinkLei() {
+		return linkLei;
+	}
+
+	public void setLinkLei(String linkLei) {
+		this.linkLei = linkLei;
+	}
+
+	public String getLinkLeiDetalhe() {
+		return linkLeiDetalhe;
+	}
+
+	public void setLinkLeiDetalhe(String linkLeiDetalhe) {
+		this.linkLeiDetalhe = linkLeiDetalhe;
+	}
+
+}
